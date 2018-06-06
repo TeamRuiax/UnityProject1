c728aecb8d03b533d14b369076b1d62edfc6a0df
JYJ <zjfps@naver.com>
2018-06-06 20:45:34 +0900

    유니티 기초 프로젝트

---
+++ MainProject/Assets/LowPolyNature/Scripts/Gun.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : InventoryItemBase {
+
+    public override void OnUse()
+    {
+        base.OnUse();
+    }
+}
